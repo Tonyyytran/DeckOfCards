507ee411ab5a4502d451c82f651ab87dcab778ca
TonyT <37555343+Tonyyytran@users.noreply.github.com>
2020-07-21 01:03:04 -0700

    Implement viewmodel to save instance on rotation

--- app/src/main/java/com/example/deckofcards/DeckViewModel.kt
+++ app/src/main/java/com/example/deckofcards/DeckViewModel.kt
@@ -1,21 +1,69 @@
 package com.example.deckofcards
 
+import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
-class DeckViewModel {
+class DeckViewModel : ViewModel() {
+
+    val cardDeck = arrayListOf<Int>(
+        R.drawable.ac, R.drawable.ad, R.drawable.ah, R.drawable.aspade,
+        R.drawable.twoc, R.drawable.twod, R.drawable.twoh, R.drawable.twos,
+        R.drawable.threec, R.drawable.threed, R.drawable.threeh, R.drawable.threes,
+        R.drawable.fourc, R.drawable.fourd, R.drawable.fourh, R.drawable.fours,
+        R.drawable.fivec, R.drawable.fived, R.drawable.fiveh, R.drawable.fives,
+        R.drawable.sixc, R.drawable.sixd, R.drawable.sixh, R.drawable.sixs,
+        R.drawable.sevenc, R.drawable.sevend, R.drawable.sevenh, R.drawable.sevens,
+        R.drawable.eightc, R.drawable.eightd, R.drawable.eighth, R.drawable.eights,
+        R.drawable.ninec, R.drawable.nined, R.drawable.nineh, R.drawable.nines,
+        R.drawable.tenc, R.drawable.tend, R.drawable.tenh, R.drawable.tens,
+        R.drawable.jc, R.drawable.jd, R.drawable.jh, R.drawable.js,
+        R.drawable.qc, R.drawable.qd, R.drawable.qh, R.drawable.qs,
+        R.drawable.kc, R.drawable.kd, R.drawable.kh, R.drawable.ks
+    )
     private val cardIndex: MutableLiveData<Int>  = MutableLiveData<Int>()
 
-//    Init {
-//        cardIndex.value = 0
-//    }
+    init {
+        cardIndex.value = 0
+    }
 
     fun getDeckIndex(): LiveData<Int> {
         return cardIndex
     }
-
-    fun resetCardIndex() {
-        cardIndex.value = 0
+    fun updateCardIndex() {
+        cardIndex.value = cardIndex.value?.plus(1)
     }
+
+    fun resetDeck(){
+        cardIndex.value = 0
+        cardDeck.shuffle()
+    }
+
+    fun drawCard() {
+        updateCardIndex()
+        if(cardIndex.value!! > 52){
+            resetDeck()
+        }
+    }
+
+    fun getCardButton() : Int {
+        return cardDeck[cardIndex.value!!]
+    }
+
+    fun getLastCard() : Int {
+        return cardDeck[cardIndex.value!! - 1]
+    }
+
+    fun getSecondLastCard() : Int {
+        return cardDeck[cardIndex.value!! - 2]
+    }
+
+    fun getThirdLastCard() : Int {
+        return cardDeck[cardIndex.value!! - 3]
+    }
+
+
 
 }
--- app/src/main/java/com/example/deckofcards/MainActivity.kt
+++ app/src/main/java/com/example/deckofcards/MainActivity.kt
@@ -13,33 +13,25 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     private var count: Int = 0
     //
-    private val cardDeck = arrayListOf<Int>(
-        R.drawable.ac, R.drawable.ad, R.drawable.ah, R.drawable.aspade,
-        R.drawable.twoc, R.drawable.twod, R.drawable.twoh, R.drawable.twos,
-        R.drawable.threec, R.drawable.threed, R.drawable.threeh, R.drawable.threes,
-        R.drawable.fourc, R.drawable.fourd, R.drawable.fourh, R.drawable.fours,
-        R.drawable.fivec, R.drawable.fived, R.drawable.fiveh, R.drawable.fives,
-        R.drawable.sixc, R.drawable.sixd, R.drawable.sixh, R.drawable.sixs,
-        R.drawable.sevenc, R.drawable.sevend, R.drawable.sevenh, R.drawable.sevens,
-        R.drawable.eightc, R.drawable.eightd, R.drawable.eighth, R.drawable.eights,
-        R.drawable.ninec, R.drawable.nined, R.drawable.nineh, R.drawable.nines,
-        R.drawable.tenc, R.drawable.tend, R.drawable.tenh, R.drawable.tens,
-        R.drawable.jc, R.drawable.jd, R.drawable.jh, R.drawable.js,
-        R.drawable.qc, R.drawable.qd, R.drawable.qh, R.drawable.qs,
-        R.drawable.kc, R.drawable.kd, R.drawable.kh, R.drawable.ks
-        )
 
-
+    private lateinit var model: DeckViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        cardDeck.shuffle()
+        model = ViewModelProviders.of(this).get(DeckViewModel::class.java)
+        model.getDeckIndex().observe(this,Observer<Int>{
+            ///// observed data
+        })
+        setCards()
         cardButton.setOnClickListener {
-            drawCard()
+            model.drawCard()
+            setCards()
         }
         resetButton.setOnLongClickListener{
-            resetDeck()
+            model.resetDeck()
+            setCards()
+            true
         }
         resetButton.setOnClickListener {
             var toast = Toast.makeText(this, "Hold to reset deck", Toast.LENGTH_SHORT)
@@ -47,50 +39,45 @@ class MainActivity : AppCompatActivity() {
             toast.show()
         }
     }
-
-    private fun drawCard() {
-        count++
-        if(count > 52){
-            count = 0
-            cardDeck.shuffle()
-            cardButton.setBackgroundResource(R.drawable.back)
-            lastCardImage.visibility = View.GONE
-            secondLastCardImage.visibility = View.GONE
-            thirdLastCardImage.visibility = View.GONE
-        }
-        else {
-            cardButton.setBackgroundResource(cardDeck[count - 1])
-            if(count == 2){
-                lastCardImage.setImageResource(cardDeck[count - 2])
+    private fun setCards() {
+        countText.text = "${model.getDeckIndex().value}/52"
+        when (model.getDeckIndex().value) {
+            0 -> {
+                cardButton.setBackgroundResource(R.drawable.back)
+                lastCardImage.visibility = View.INVISIBLE
+                secondLastCardImage.visibility = View.INVISIBLE
+                thirdLastCardImage.visibility = View.INVISIBLE
+            }
+            1 -> {
+                cardButton.setBackgroundResource(model.getCardButton())
+                lastCardImage.visibility = View.INVISIBLE
+                secondLastCardImage.visibility = View.INVISIBLE
+                thirdLastCardImage.visibility = View.INVISIBLE
+            }
+            2 -> {
+                cardButton.setBackgroundResource(model.getCardButton())
+                lastCardImage.setImageResource(model.getLastCard())
                 lastCardImage.visibility = View.VISIBLE
+                secondLastCardImage.visibility = View.INVISIBLE
+                thirdLastCardImage.visibility = View.INVISIBLE
             }
-            else if(count == 3){
-                lastCardImage.setImageResource(cardDeck[count - 2])
-                secondLastCardImage.setImageResource(cardDeck[count - 3])
+            3 -> {
+                cardButton.setBackgroundResource(model.getCardButton())
+                lastCardImage.setImageResource(model.getLastCard())
+                lastCardImage.visibility = View.VISIBLE
+                secondLastCardImage.setImageResource(model.getSecondLastCard())
                 secondLastCardImage.visibility = View.VISIBLE
+                thirdLastCardImage.visibility = View.INVISIBLE
             }
-            else if(count == 4){
-                lastCardImage.setImageResource(cardDeck[count - 2])
-                secondLastCardImage.setImageResource(cardDeck[count - 3])
-                thirdLastCardImage.setImageResource(cardDeck[count - 4])
+            else -> {
+                cardButton.setBackgroundResource(model.getCardButton())
+                lastCardImage.setImageResource(model.getLastCard())
+                lastCardImage.visibility = View.VISIBLE
+                secondLastCardImage.setImageResource(model.getSecondLastCard())
+                secondLastCardImage.visibility = View.VISIBLE
+                thirdLastCardImage.setImageResource(model.getThirdLastCard())
                 thirdLastCardImage.visibility = View.VISIBLE
             }
-            else if(count > 4){
-                lastCardImage.setImageResource(cardDeck[count - 2])
-                secondLastCardImage.setImageResource(cardDeck[count-3])
-                thirdLastCardImage.setImageResource(cardDeck[count-4])
-            }
         }
-        countText.text = "$count/52"
-    }
-
-    private fun resetDeck(): Boolean {
-        count = 0
-        cardDeck.shuffle()
-        cardButton.setBackgroundResource(R.drawable.back)
-        lastCardImage.visibility = View.GONE
-        secondLastCardImage.visibility = View.GONE
-        thirdLastCardImage.visibility = View.GONE
-        return true
     }
 }
